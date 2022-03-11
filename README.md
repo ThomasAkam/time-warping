e017322565108fba96b9e328afe6b0c2cba7215f
Thomas Akam <thomas.akam@gmail.com>
2022-03-11 18:50:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Time-warping for trial alignment of neural activity.
+
+This repository contains Python code for time-warping neurophysiological data to align activity across trials of a behavioral task.  It contains two functions *align_spikes* and *align_signals* for warping discrete spike data and continuous data respectively.  Both functions work by linearly warping activity between a set of reference points of interest, e.g. the times of trial initiation and choice, to align activity on each trial onto a single set of reference time points, usually the median timings across trials.  See the docstrings in the code for details of how to use each function. 
+
+##  Align spikes
+
+The *align_spikes* function is used to align spike activity in the form of spike times and corresponding neuron IDs.   Its is described [Samborska et al. bioRxiv 2021](https://www.biorxiv.org/content/10.1101/2021.03.05.433967v2) and illustrated in figure S4 of that manuscript, reproduced below.  
+
+![align_spikes](media\align_spikes.png)
+
+**Figure legend:** Diagram illustrating alignment of spike activity across trials. Trials were aligned using the times of initiation and choice port entry, by warping the interval between these two events to match the median interval. Top panels show spike times in the true (blue ticks) and aligned (orange ticks) time reference frames. Spike times were transformed by linear interpolation between the reference points. The output firing rate (bottom panels) was calculated at points spaced every 40ms in the aligned reference frame using Gaussian smoothing (40ms standard deviation) of the spike train. To compensate for the change in spike density due to the time warping, spikes were weighed by the stretch factor between the true and aligned reference frames (weighting is indicated by height of the orange ticks) prior to Gaussian smoothing. 
+
+## Align signals
+
+The *align_signals* function is used to align continuous data, which could be photometry or imaging signals, or the output of motion tracking software such as DeepLabCut.  It is described in [Akam et al. Neuron 2021](https://www.cell.com/neuron/fulltext/S0896-6273(20)30809-6) and illustrated in figure S4 of that manuscript, reproduced below.
+
+![align_spikes](media\align_signals.png)
+
+
+
+**Top panels:** Alignment of imaging data on a trial where the interval between choice and second-step port entry was longer than the median interval. Left panel shows the true and aligned times of microscope frames plotted against each other. Right top panel shows the activity of 5 neurons before alignment. Vertical dashed lines show the times of choice and second-step port entry. Right bottom panel shows the activity of the same 5 neurons after alignment, up-sampling and smoothing. Grey shaded regions indicate the interval between choice and second-step port entry that is time-warped **Bottom panels:** As above but for a trial where the interval between choice and second-step port entry was shorter than the median interval. 
---
+++ align_activity.py
@@ -0,0 +1,254 @@
+'''Code for time warping neurophysiology data to align activity across 
+trials.  The align_spikes function is used to align spike activity in
+the form of neuron IDs corresponding spike times. The align_signals 
+function is used to align continuous data, e.g. photometry signals or 
+motion tracking data from DeepLabCut. 
+
+Copyright (c) Thomas Akam 2022. Licenced under the GNU General Public License v3.
+'''
+
+import numpy as np
+import pylab as plt
+from scipy.stats import norm
+from scipy.special import erf
+
+# Set plotting paramters.
+
+plt.rcParams['pdf.fonttype'] = 42
+plt.rc("axes.spines", top=False, right=False)
+
+# --------------------------------------------------------------------------------
+# Align spikes
+# --------------------------------------------------------------------------------
+
+def align_spikes(trial_times, target_times, spikes, pre_win=0, post_win=0, fs_out=25,
+                 smooth_SD='default', plot=False):
+    '''Calculate trial aligned smoothed firing rates from spike time data. Spike times
+    are first transformed from the original time frame to a trial aligned time frame in
+    which a set of reference time points for each trial are mapped onto a set of target
+    time points (e.g. the median trial timings), with linear interpolation of spike times
+    between the reference points. Once the spike times have been transformed into the 
+    trial aligned reference frame the firing rate is calculated at a specified sampling
+    rate, using Gaussian smoothing with a specified standard deviation, taking into
+    acount the change in spike density due to the time warping.  The pre_win and post_win
+    arguments can be used to specify time windows before the first and after the last
+    alignment event on each trial to be included in the output signals.
+
+    Arguments:
+    trial_times : Array of reference point times for each trial (ms) [n_trials, n_ref_points]
+    target_times: Reference point times to warp each trial onto (ms) [n_ref_points]
+    spikes:  Array of neuron IDs and spike times [2, n_spikes]
+             spikes[0,:] is neuron IDs, spikes [1,:] is spike times (ms).
+    pre_win: Time window before first event to include in aligned rates (ms).
+    post_win: Time window after last event to include in aligned rates (ms).
+    fs_out: Sampling rate of output firing rate (Hz).
+    smooth_SD: Standard deviation of gaussian smoothing applied to ouput rate (ms). 
+               If set to default, smooth_SD is set to the inter sample interval.
+    plot: If set to True, plots the time warping for the most active neuron on each trial.
+          of the most active neurons activity.
+    Returns dictionary with items:
+    aligned_rates: Array of trial aligned smoothed firing rates (Hz) [n_trials, n_neurons, n_timepoints].
+    t_out: Times of each output firing rate time point (ms).
+    min_max_stretch: Minimum and maximum stretch factor for each trial.  Used to exclude 
+                     trials which have extreme deviation from target timings [n_trials, 2]
+    '''
+    if smooth_SD == 'default': smooth_SD = 1000/fs_out
+    n_trials = trial_times.shape[0]
+    neuron_IDs = np.sort(np.unique(spikes[0,:]))
+    n_neurons = len(neuron_IDs)
+    t_out = np.arange(target_times[0]-pre_win, target_times[-1]+post_win, 1000/fs_out) # Timepoints of output samples.
+    pad_len = smooth_SD*4 
+    # Add non-warped interval before and after specified intervals to prevent edge effects and 
+    # include the pre and post windows if specified.
+    target_times = np.hstack([target_times[0]      -pre_win-pad_len, target_times, target_times[-1]      +post_win+pad_len])
+    trial_times  = np.hstack([trial_times[:,0,None]-pre_win-pad_len, trial_times , trial_times[:,-1,None]+post_win+pad_len])
+    # Apply timewarping.
+    target_deltas = np.diff(target_times) # Intervals between target time points.
+    trial_deltas = np.diff(trial_times,1) # Intervals between reference points for each trial.
+    stretch_factors = target_deltas/trial_deltas # Amount each interval of each trial must be stretched/squashed by.
+    min_max_stretch = np.vstack([np.min(stretch_factors,1), np.max(stretch_factors,1)]).T # Minimum and maximum stretch factor for each trial.
+    aligned_rates = np.zeros([n_trials, n_neurons, len(t_out)]) # Array to store trial aligned firing rates. 
+    for tr in range(n_trials): # Loop over trials.
+        trial_spikes = spikes[:, (trial_times[tr,0] < spikes[1,:]) & 
+                                 (spikes[1,:] < trial_times[tr,-1])]
+        trial_spike_IDs = trial_spikes[0,:]
+        trial_spike_times = trial_spikes[1,:]
+        aligned_sp_times = trial_spike_times.copy()
+        # Change times of trial_spikes to map them onto target_times.
+        spike_stretch = np.zeros(trial_spikes.shape[1]) # Stretch factor for each spike.
+        for i in range(len(target_times)-1): # Loop over intervals.
+            interval_mask = ((trial_times[tr,i] < trial_spike_times) &  # Boolean mask indicating which spikes are in interval i.
+                             (trial_spike_times < trial_times[tr,i+1])) 
+            aligned_sp_times[interval_mask] = target_times[i] + stretch_factors[tr,i] * (
+                                            aligned_sp_times[interval_mask] - trial_times[tr,i])
+            spike_stretch[interval_mask] = stretch_factors[tr,i]
+        for j, n in enumerate(neuron_IDs): # Loop over neurons.
+            if n in trial_spike_IDs:
+                neuron_mask = trial_spike_IDs == n
+                n_spike_times = aligned_sp_times[neuron_mask]
+                aligned_rates[tr,j,:] = 1000*np.sum(norm.pdf(n_spike_times[None,:]
+                    -t_out[:,None], scale=smooth_SD)*spike_stretch[neuron_mask],1)
+        if plot: # Plot trial alignment for neuron most active on trial.
+            plt.figure(1, figsize=[4.6,7], clear=True)
+            fig, ax = plt.subplots(3, 1, gridspec_kw={'height_ratios': [5,1,2]}, num=1)
+            # Plot spike times in input and output reference frames.
+            plt.sca(ax[0])
+            most_active = np.argmax(np.mean(aligned_rates[tr,:,:],axis=1))
+            neuron_mask = trial_spike_IDs == neuron_IDs[most_active]
+            n_spikes = np.sum(neuron_mask)
+            ylim = [trial_times[tr,0]-trial_times[tr,1]+pad_len, trial_times[tr,-1]-trial_times[tr,1]-pad_len]
+            plt.scatter(np.ones(n_spikes)*t_out[0], trial_spike_times[neuron_mask]-trial_times[tr,1], s=60, marker=1)
+            plt.scatter(aligned_sp_times[neuron_mask], np.ones(n_spikes)*ylim[0],
+                        s=60*spike_stretch[neuron_mask], marker=2)
+            plt.plot(target_times,trial_times[tr,:]-trial_times[tr,1],'.:', c='k')
+            plt.ylabel('True time (ms)')
+            plt.xlim(t_out[0], t_out[-1])
+            plt.ylim(*ylim)
+            # Plot Gaussians used for smoothing output firing rate.
+            plt.sca(ax[1]) 
+            t_g = np.arange(t_out[0], t_out[-1])
+            for t in t_out:
+                g_max = 1/norm.pdf(0, scale=smooth_SD)
+                plt.plot(t_g, g_max*norm.pdf(t-t_g, scale=smooth_SD), c='grey', linewidth=0.5)
+            for t in target_times[1:-1]:
+                plt.axvline(t, color='k', linestyle=':')
+            plt.xlim(t_out[0], t_out[-1])
+            plt.tick_params(labelbottom=False, labelleft=False)
+            # Plot output firing rate.
+            plt.sca(ax[2]) 
+            plt.plot(t_out,aligned_rates[tr,most_active,:])
+            for t in target_times[1:-1]:
+                plt.axvline(t, color='k', linestyle=':')
+            plt.xlim(t_out[0], t_out[-1])
+            plt.ylim(ymin=0)
+            plt.xlabel('Aligned time (ms)')
+            plt.ylabel('Firing rate (Hz)')
+            plt.tight_layout()
+            plt.pause(0.05)
+            if input("Press enter for next trial, 'x' to stop plotting:") == 'x':
+                plot = False
+    return {'aligned_rates'   : aligned_rates,
+            't_out'           : t_out,
+            'min_max_stretch' : min_max_stretch}
+
+# --------------------------------------------------------------------------------
+# Align signals
+# --------------------------------------------------------------------------------
+
+def align_signals(signals, sample_times, trial_times, target_times, pre_win=0, post_win=0,
+                  fs_out=25, smooth_SD='auto', plot_warp=False):
+    '''
+    Timewarp continuous signals to align event times on each trial to specified target
+    event times. For each trial, input sample times are linearly time warped to align
+    that trial's event times with the target times.  Activity is then evaluated at a set of
+    regularly spaced timepoints relative to the target event times by linearly interpolating
+    signals between input samples followed by Gaussian smoothing around output timepoints.
+    This allows a single mathematical operation to handle both time streching (where 
+    interpolation in needed) and time compression (where averaging is needed). The pre_win
+    and post_win arguments can be used to specify time windows before the first and after
+    the last alignment event on each trial to be included in the output signals.
+
+    Arguments: 
+    signals      : Signals to be aligned [n_signals, n_samples]
+    sample_times : Times when the samples occured (ms) [n_samples]
+    trial_times  : Times of events used for alignment for each trial (ms) [n_trials, n_events]
+    target_times : Times of events used for alignment in output aligned trial (ms) [n_events].
+    pre_win      : Time window before first event to include in aligned signals (ms).
+    post_win     : Time window after last event to include in aligned signals (ms).
+    fs_out       : The sampling rate of the aligned output signals (Hz).
+    smooth_SD    : Standard deviation (ms) of Gaussian smoothing applied to output signals.
+                   If set to 'auto', smooth_SD is set to 1000/fs_out.
+    plot_warp    : If True the input and output signals are plotted for the most active 
+                   neurons for each trial.
+
+    Returns dictionary with items:
+    aligned_signals : Array of trial aligned signals [n_trials, n_signals, n_timepoints]
+    t_out: Times of each output firing rate time point (ms).
+    min_max_stretch: Minimum and maximum stretch factor for each trial.  Used to exclude 
+                     trials which have extreme deviation from target timings [n_trials, 2]
+   '''
+    assert not np.any(np.diff(trial_times,1)<0), 'trial_times give negative interval duration'
+    assert not np.any(np.diff(target_times)<0) , 'target_times give negative interval duration'
+
+    if smooth_SD=='auto': smooth_SD = 1000/fs_out
+
+    t_out = np.arange(target_times[0]-pre_win, target_times[-1]+post_win, 1000/fs_out) # Timepoints of output samples.
+    pad_len = smooth_SD*4 # Extension to alignement interval to prevent edge effects.
+
+    # Add non-warped interval before and after specified intervals to prevent edge effects and 
+    # include the pre and post windows if specified.
+
+    target_times = np.hstack([target_times[0]      -pre_win-pad_len, target_times, target_times[-1]      +post_win+pad_len])
+    trial_times  = np.hstack([trial_times[:,0,None]-pre_win-pad_len, trial_times , trial_times[:,-1,None]+post_win+pad_len])
+    
+    # Apply timewarping.
+
+    target_deltas = np.diff(target_times) # Duration of inter event intervals for aligned signals (ms).
+    trial_deltas = np.diff(trial_times,1) # Duration of inter event intervals for each trial (ms).
+
+    stretch_factors = target_deltas/trial_deltas # Amount each interval of each trial must be stretched/squashed by.
+    min_max_stretch = np.vstack([np.min(stretch_factors,1), np.max(stretch_factors,1)]).T # Minimum and maximum stretch factor for each trial.
+
+    n_trials, n_signals, n_timepoints = (trial_times.shape[0], signals.shape[0], len(t_out))
+    aligned_signals = np.full([n_trials, n_signals, n_timepoints], np.nan)
+    
+    for i in np.arange(n_trials):
+        if trial_times[i, 0] < sample_times[0]:
+            continue # This trial occured before signals started. 
+        if trial_times[i,-1] > sample_times[-1]:
+            break    # This and subsequent trials occured after signals finshed.
+
+        # Linearly warp sample times to align inter event intervals to target.
+        trial_samples = ((trial_times[i,0] <= sample_times) & (sample_times < trial_times[i,-1]))
+        trial_signals = signals[:,np.where(trial_samples)[0]]
+        t0 = sample_times[trial_samples]  # Trial sample times before warping
+        t1 = np.zeros(len(t0))          # Trial sample times after warping
+        for j in range(target_deltas.shape[0]):
+            mask = (trial_times[i,j] <= t0) & (t0 < trial_times[i,j+1])
+            t1[mask] = (t0[mask]-trial_times[i,j])*target_deltas[j]/trial_deltas[i,j] + target_times[j]
+
+        # Calculate aligned signals using analytical solution to overlap integral between
+        # linearly interpolated signals and gaussian around output timepoints.
+        aligned_signals[i,:,:] = (np.sum(_int_norm_lin_prod(trial_signals[:,:-1],
+            trial_signals[:,1:],t1[:-1],t1[1:],t_out[:,None,None],s=smooth_SD),2).T)
+
+        if plot_warp: # Plot input and output signals for the most active neurons.
+            most_active = np.argsort(np.mean(trial_signals,1))[-5:]
+            plt.figure(2, figsize=[10,3.2]).clf()
+            plt.subplot2grid((1,3),(0,0))
+            plt.plot(t0,t1,'.-')
+            plt.ylabel('Aligned trial time (ms)')
+            plt.xlabel('True trial time (ms)')
+            plt.subplot2grid((2,3),(0,1), colspan=2)
+            plt.plot(t0, trial_signals[most_active,:].T,'.-')
+            for x in trial_times[i,:]:
+                plt.axvline(x, color='k', linestyle=':')
+            plt.xlim(trial_times[i,1]-pre_win, trial_times[i,-2]+post_win)
+            plt.ylabel('Activity')
+            plt.xlabel('True trial time (ms)')
+            plt.subplot2grid((2,3),(1,1), colspan=2)
+            plt.plot(t_out,aligned_signals[i,most_active,:].T,'.-')
+            for x in target_times:
+                plt.axvline(x, color='k', linestyle=':')
+            plt.xlim(t_out[0], t_out[-1])
+            plt.ylabel('Activity')
+            plt.xlabel('Aligned trial time (ms)')
+            plt.tight_layout()
+            plt.pause(0.05)
+            if input("Press enter for next trial, 'x' to stop plotting:") == 'x':
+                plot_warp = False
+
+    return {'aligned_signals' : aligned_signals, 
+            't_out'           : t_out,           
+            'min_max_stretch' : min_max_stretch}
+
+
+r2pi = np.sqrt(2*np.pi)
+r2   = np.sqrt(2) 
+
+def _int_norm_lin_prod(a,b,v,t,u,s):
+    '''Evaluate the integral w.r.t. x of (a+(b-a)*(x-v)/(t-v))*Npdf(u,s) from v to t where 
+    Npdf is the Normal distribution probability density function. Wolfram Integrator: 
+    integrate ((a+(b-a)*(x-v)/(t-v))/(s*sqrt(2*pi)))*exp(-((x-u)^2)/(2*s^2)) dx from v to t'''
+    return (1/(2*r2pi*(t-v)))*(r2pi*(a*(t-u)+b*(u-v))*(erf((t-u)/(r2*s))-erf((v-u)/(r2*s)))+
+            2*s*(a-b)*(np.exp(-((t-u)**2)/(2*s**2))-np.exp(-((v-u)**2)/(2*s**2))))
